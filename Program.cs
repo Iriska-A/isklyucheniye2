c53307a066126b6d0ea8a50a2c4c863835ae6953
Iriska-A <72428513+Iriska-A@users.noreply.github.com>
2020-10-15 13:18:26 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите строку");
+                string message = Console.ReadLine();
+
+                if (message.Length > 10)
+                {
+                    throw new Exception("Длина строки больше 10 символов");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Возникла ошибка:длина строки больше 10 символов");
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally");
+            }
+            Console.WriteLine("Завершение программы");
+            Console.Read();
+        }
+    }
+}
